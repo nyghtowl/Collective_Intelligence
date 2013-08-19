4b5bdcf5c147b28a5230ce6e017394e3099a223a
nyghtowl <warrick.melanie@gmail.com>
2013-08-19 11:51:47 -0700

    Added drawing dendrogram.

--- discover_groups/clusters.py
+++ discover_groups/clusters.py
@@ -7,6 +7,7 @@ Hierarchical Clustering
 
 '''
 from math import sqrt
+from PIL import Image,ImageDraw
 
 # Load data file
 def readfile(filename):
@@ -43,15 +44,6 @@ def pearson(v1, v2):
     if den == 0: return 0
 
     return 1.0-num/den # Return smaller distance between items that are more similar
-
-# Hierarchical tree & properties
-class bicluster:
-    def __init__(self,vec,left=None,right=None,distance=0.0,id=None):
-        self.left=left
-        self.right=right
-        self.vec=vec
-        self.id=id
-        self.distance=distance
 
 # Creates hierarchical cluster
 def hcluster(rows, distance=pearson):
@@ -92,6 +84,15 @@ def hcluster(rows, distance=pearson):
 
     return clust[0]
 
+# Hierarchical tree & properties
+class bicluster:
+    def __init__(self,vec,left=None,right=None,distance=0.0,id=None):
+        self.left=left
+        self.right=right
+        self.vec=vec
+        self.id=id
+        self.distance=distance
+
 def printclust(clust, labels=None, n=0):
     # Indent to make a hierarchy layout
     for i in range(n): print ' ',
@@ -106,3 +107,62 @@ def printclust(clust, labels=None, n=0):
     # Now print the right and left branches
     if clust.left!=None: printclust(clust.left,labels=labels,n=n+1)
     if clust.right!=None: printclust(clust.right,labels=labels,n=n+1)
+
+# Return total cluster height
+def getheight(clust):
+    # Is this an endpoint? Then the height is just 1
+    if clust.left==None and clust.right==None: return 1
+
+    # Otherwise the height is the same of the heights of each branch
+    return getheight(clust.left)+getheight(clust.right)
+
+# Return total cluster depth
+def getdepth(clust):
+    # The distance of an endpoint is 0.0
+    if clust.left==None and clust.righ==None: return 0
+    # The distance of a branch is the greater of its two sides plus its own distance
+    return max(getdepth(clust.left),getdepth(clust.right))+clust.distance
+
+# New image created with boundaries in width and height
+def drawdendrogram(clust, labels, jpeg='clusters.jpg'):
+    # Height and width
+    h=getheight(clust)*20
+    w=1200
+    depth=getdepth(clust)
+
+    # Width is fixed, so scale distances accordingly
+    scaling=float(w-150)/depth
+
+    # Create a new image with a white background
+    img=Image.new('RGB',(w,h),(255,255,255))
+    draw=ImageDraw.Draw(img)
+
+    draw.line((0,h/2,10,h/2),fill=(255,0,0))
+
+    # Draw the first node
+    drawnode(draw,clust,10,(h/2),scaling,labels)
+    img.save(jpeg,"JPEG")
+
+def drawnode(draw, clust, x, y, scaling, labels):
+    if clust.id<0:
+        h1=getheight(clust.left)*20
+        h2=getheight(clust.right)*20
+        top=y-(h1+h2)/2
+        bottom=y+(h1+h2)/2
+        # Line length
+        ll=clust.distance*scaling
+        #Vertical line from this cluster to children
+        draw.line((x,top+h1/2,x,bottom-h2/2),fill=(255,0,0))
+
+        # Horizontal line to left item
+        draw.line((x,top+h1/2,x+ll,top+h1/2),fill=(255,0,0))
+
+        # Horizontal line to right item
+        draw.line((x,bottom-h2/2,x+ll,bottom-h2/2),fill=(255,0,0))
+
+        # Call the function to draw the left and right nodes
+        drawnode(draw,clust.left,x+ll,top+h1/2,scaling,labels)
+        drawnode(draw,clust.right,x+ll,bottom-h2/2,scaling,labels)
+    else:
+        # If this is an endpoint, draw the item label
+        draw.text((x+5,y-7),labels[clust.id],(0,0,0))
